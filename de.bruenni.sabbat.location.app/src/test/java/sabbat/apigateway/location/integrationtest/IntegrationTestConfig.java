d2f534b173e049d738f0a302a4b3bacac57e2516
Olli B <oliver.bruentje@gmx.de>
2016-09-07 18:52:51 +0200

    Added location.app project -> service hosting location core & infrastructure to stream measurment values and handle activity management

---
+++ de.bruenni.sabbat.location.app/src/test/java/sabbat/apigateway/location/integrationtest/IntegrationTestConfig.java
@@ -0,0 +1,14 @@
+package sabbat.apigateway.location.integrationtest;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Created by bruenni on 13.07.16.
+ */
+@Configuration
+@PropertySource("classpath:test/application.properties")
+@ImportResource(locations = {"classpath:test/spring-api-gateway-test.xml"})
+public class IntegrationTestConfig {
+}
---
+++ de.bruenni.sabbat.location.app/src/test/java/sabbat/apigateway/location/unittest/UnitTestConfig.java
@@ -0,0 +1,19 @@
+package sabbat.apigateway.location.unittest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+/**
+ * Created by bruenni on 13.07.16.
+ */
+@Configuration
+@PropertySource("classpath:test/application.properties")
+@ImportResource("classpath:test/spring-api-gateway-test.xml")
+public class UnitTestConfig {
+}
