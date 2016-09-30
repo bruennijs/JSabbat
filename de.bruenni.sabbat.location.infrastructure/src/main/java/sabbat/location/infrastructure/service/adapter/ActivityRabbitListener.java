3857174d914d73a266c2c2773f9a259ed147211e
Olli B <oliver.bruentje@gmx.de>
2016-09-30 22:05:05 +0200

    Changed ActivityRabbitListener to synchronous method first

--- de.bruenni.sabbat.location.infrastructure/src/main/java/sabbat/location/infrastructure/service/adapter/ActivityRabbitListener.java
+++ de.bruenni.sabbat.location.infrastructure/src/main/java/sabbat/location/infrastructure/service/adapter/ActivityRabbitListener.java
@@ -14,9 +14,12 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureAdapter;
+import rx.Observable;
+import rx.Observer;
 import sabbat.location.core.application.service.IActivityApplicationService;
 import sabbat.location.core.application.service.command.ActivityCreateCommand;
 import sabbat.location.core.domain.model.Activity;
@@ -27,6 +30,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -63,10 +67,10 @@ public class ActivityRabbitListener {
     @RabbitListener(id="activityListener",
                     queues = "${location.activity.queue.command}")
     //@Header("correlationId") String correlationId
-    public ListenableFuture<Message> onMessage(Message message,
-                             @Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String routingKey,
-                             Channel channel,
-                             @Header(AmqpHeaders.CORRELATION_ID) byte[] correlationId) throws Exception {
+    public Message onMessage(Message message,
+                                                       @Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String routingKey,
+                                                       Channel channel,
+                                                       @Header(AmqpHeaders.CORRELATION_ID) byte[] correlationId) throws Exception {
         logger.debug("-----> " + message.toString() + "cid=" + correlationId + ",routingkey=" + routingKey);
 
         if (routingKey.equals(ActivityStartRoutingKey)) {
@@ -77,8 +81,17 @@ public class ActivityRabbitListener {
 
             ListenableFuture<Activity> activityStartFuture = this.applicationService.start(command);
 
-            //Activity activity = activityStartFuture.get(1000, TimeUnit.MILLISECONDS);
-            return new ListenableFutureAdapter<Message, Activity>(activityStartFuture) {
+            //Observable.from(activityStartFuture)
+            ActivityCreatedResponseDto dtoResponse = new ActivityCreatedResponseDto(activityStartFuture.get().getKey().getId().toString());
+
+            MessageProperties msgProps = MessagePropertiesBuilder.newInstance().setCorrelationId(correlationId).build();
+            Message response = MessageBuilder.withBody(dtoParser.serialize(dtoResponse).getBytes())
+                    .andProperties(msgProps)
+                    .build();
+
+            return response;
+
+/*            return new ListenableFutureAdapter<Message, Activity>(activityStartFuture) {
                 @Override
                 protected Message adapt(Activity activity) throws ExecutionException {
                     try {
@@ -96,11 +109,10 @@ public class ActivityRabbitListener {
                         logger.error("transforming of activity failed [" + activity.toString() + "]", e);
                         throw new ExecutionException(e);
                     }
-                }
-            };
+                }*/
+            }
 
             //channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
-        }
 
         throw new Exception("not implemented");
     }
