7991f168416194ab9d6c7f477de260f84a9c6d5f
Olli B <oliver.bruentje@gmx.de>
2017-06-01 21:26:41 +0200

    get location app into running state

--- de.bruenni.sabbat.messenger.core/src/test/java/domain/messenger/aggregates/MessageTest.java
+++ de.bruenni.sabbat.messenger.core/src/test/java/domain/messenger/aggregates/MessageTest.java
@@ -2,6 +2,7 @@ package domain.messenger.aggregates;
 
 import builder.DeliveryResponseBuilder;
 import builder.MessageBuilder;
+import infrastructure.common.event.Event;
 import infrastructure.common.event.IEvent;
 import infrastructure.common.gateway.AggregateCorrelationException;
 import org.junit.Test;
@@ -20,7 +21,7 @@ public class MessageTest {
     @Test
     public void When_message_handling_response_expect_MessageresponseReceivedEvent_returned() throws URISyntaxException, AggregateCorrelationException {
         Message sut = new MessageBuilder().Build();
-        IEvent event = sut.onDeliveryResponse(new DeliveryResponseBuilder().Build());
+        Event event = sut.onDeliveryResponse(new DeliveryResponseBuilder().Build());
 
         Assert.isInstanceOf(DeliveryResponseReceivedEvent.class, event);
     }
@@ -28,7 +29,7 @@ public class MessageTest {
     @Test
     public void When_handling_MessageresponseReceivedEvent_expect_state_transition_to_delivered() throws AggregateCorrelationException {
         Message sut = new MessageBuilder().Build();
-        IEvent event = sut.onDeliveryResponse(new DeliveryResponseBuilder().Build());
+        Event event = sut.onDeliveryResponse(new DeliveryResponseBuilder().Build());
         sut.OnEvent(event);
         org.junit.Assert.assertEquals(MessageState.Delivered, sut.getState());
         sut.OnEvent(event);
@@ -38,10 +39,10 @@ public class MessageTest {
     @Test
     public void When_handling_MessageresponseReceivedEvent_expect_no_state_state_transition_to_delivered_with_response_failure() throws AggregateCorrelationException {
         Message sut = new MessageBuilder().Build();
-        IEvent event = sut.onDeliveryResponse(new DeliveryResponseBuilder().WithSuccess(false).Build());
+        Event event = sut.onDeliveryResponse(new DeliveryResponseBuilder().WithSuccess(false).Build());
         sut.OnEvent(event);
         org.junit.Assert.assertEquals(MessageState.New, sut.getState());
-        IEvent event2 = sut.onDeliveryResponse(new DeliveryResponseBuilder().WithSuccess(true).Build());
+        Event event2 = sut.onDeliveryResponse(new DeliveryResponseBuilder().WithSuccess(true).Build());
         sut.OnEvent(event2);
         org.junit.Assert.assertEquals(MessageState.Delivered, sut.getState());
     }
@@ -49,10 +50,10 @@ public class MessageTest {
     @Test
     public void When_handling_resuestreponse_and_DeliveryRequestResponse_expect_delivered_state() {
         Message sut = new MessageBuilder().Build();
-        IEvent event1 = sut.onDeliveryRequestResult(new DeliveryRequestResult("notchecked", true));
+        Event event1 = sut.onDeliveryRequestResult(new DeliveryRequestResult("notchecked", true));
         sut.OnEvent(event1);
         org.junit.Assert.assertEquals(MessageState.Pending, sut.getState());
-        IEvent event2 = sut.onDeliveryResponse(new DeliveryResponseBuilder().WithSuccess(true).Build());
+        Event event2 = sut.onDeliveryResponse(new DeliveryResponseBuilder().WithSuccess(true).Build());
         sut.OnEvent(event2);
         org.junit.Assert.assertEquals(MessageState.Delivered, sut.getState());
     }
@@ -61,10 +62,10 @@ public class MessageTest {
     @Test
     public void When_handling_DeliveryResponse_with_failure_expect_pending_state() {
         Message sut = new MessageBuilder().Build();
-        IEvent event1 = sut.onDeliveryRequestResult(new DeliveryRequestResult("notchecked", true));
+        Event event1 = sut.onDeliveryRequestResult(new DeliveryRequestResult("notchecked", true));
         sut.OnEvent(event1);
         org.junit.Assert.assertEquals(MessageState.Pending, sut.getState());
-        IEvent event2 = sut.onDeliveryResponse(new DeliveryResponseBuilder().WithSuccess(false).Build());
+        Event event2 = sut.onDeliveryResponse(new DeliveryResponseBuilder().WithSuccess(false).Build());
         sut.OnEvent(event2);
         org.junit.Assert.assertEquals(MessageState.Pending, sut.getState());
     }
@@ -72,10 +73,10 @@ public class MessageTest {
     @Test
     public void When_handling_MessageresponseReceivedEvent_expect_deliveredOn_set_to_event_timestamp() throws AggregateCorrelationException {
         Message sut = new MessageBuilder().Build();
-        IEvent event = sut.onDeliveryResponse(new DeliveryResponseBuilder().Build());
+        Event event = sut.onDeliveryResponse(new DeliveryResponseBuilder().Build());
 
         sut.OnEvent(event);
-        org.junit.Assert.assertEquals(event.getTimestamp(), sut.getDeliveredOn());
+        org.junit.Assert.assertEquals(event.getCreatedOn(), sut.getDeliveredOn());
     }
 
     @Test
