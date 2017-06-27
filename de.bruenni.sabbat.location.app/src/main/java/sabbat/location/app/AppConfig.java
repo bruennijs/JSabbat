7d7f7d7ba93d0510d1b739b4d0f014f5eae7a151
Olli B <oliver.bruentje@gmx.de>
2017-06-27 11:35:18 +0200

    force caffeine version in location.app and get it to run without version conflicts

--- de.bruenni.sabbat.location.app/src/main/java/sabbat/location/app/AppConfig.java
+++ de.bruenni.sabbat.location.app/src/main/java/sabbat/location/app/AppConfig.java
@@ -1,15 +1,7 @@
 package sabbat.location.app;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Exchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by bruenni on 07.09.16.
--- de.bruenni.sabbat.location.app/src/main/java/sabbat/location/app/Application.java
+++ de.bruenni.sabbat.location.app/src/main/java/sabbat/location/app/Application.java
@@ -1,24 +1,10 @@
 package sabbat.location.app;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
-import org.springframework.amqp.core.AcknowledgeMode;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerEndpoint;
-import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.SpringApplication;
 import org.slf4j.Logger;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
 import org.springframework.context.ApplicationContext;
-import sabbat.location.core.LocationCoreConfiguration;
-import sabbat.location.infrastructure.AmqpClientAutoConfiguration;
-import sabbat.location.infrastructure.AmqpServiceAutoConfiguration;
 
 import java.util.Arrays;
 
